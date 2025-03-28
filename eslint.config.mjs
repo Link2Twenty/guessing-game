5c391169c9573919738150c33c097be154483806
Andrew Bone <AndrewB05@gmail.com>
2025-03-28 22:44:57 +0000

    🧑‍💻 add typescript-eslint/eslint-plugin back

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,17 +2,19 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 import prettier from 'eslint-plugin-prettier'
 import reactHooks from 'eslint-plugin-react-hooks'
 import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
 
 export default [
   {
     files: ["src/**/*.{js,jsx,ts,tsx}"],
-    plugins: { 'react-hooks': reactHooks, prettier, },
+    plugins: { 'react-hooks': reactHooks, '@typescript-eslint': tsPlugin, prettier },
     languageOptions: { parser: tsParser, },
     rules: {
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
       'prettier/prettier': 'warn',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      ...tsPlugin.configs.recommended.rules,
     },
   },
-  eslintConfigPrettier,
+  eslintConfigPrettier
 ]
